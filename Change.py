5a68162e00f4d031fcd2575395fdc1dd197c1331
Hassan Gelle <hassangelle45@hotmail.com>
2021-04-27 10:44:34 +0200

    made a whole new python, do to lack off accessibility on the previos one.

--- Change.py
+++ Change.py
@@ -1,26 +1,97 @@
-def change(cost, money):
-    #money is the money that is returned
-    coins={'penny':0.01, 'nickel':0.05, 'dime':0.1,'quarter':0.25}
-    num_quarters=0
-    num_dimes=0
-    num_nickels=0
-    num_pennies=0
-    change=money-cost
-    if int(change) >= 1.0:
-        num_quarters+=int(change/coins['quarter'])
-        change-=num_quarters*0.25
-    if round(change, 1) >=0.10:
-        num_dimes+=round(change,1)/coins['dime']
-        change-=num_dimes*0.1
-    if round(change,2) <= 0.1 and round(change,2) >=0.05:
-        num_nickels+=round(change, 2)/coins['nickel']
-        change-=num_nickels*0.05
-    if round(change,2) <=0.05 and round(change,2) >=0.01:
-        num_pennies+=round(change,2)/coins['penny']
-        change-=num_pennies*0.01
-    print('quarters: ' + str(num_quarters))
-    print('dimes: ' + str(num_dimes))
-    print('nickels: '+ str(num_nickels))
-    print('pennies: '+ str(num_pennies))
-    
-        
+4#!/usr/bin/env python3
+
+# Change Calculator
+# Calculates the change for US dollar
+# Prints out the type of bills and coins
+# that needs to be given to the customer
+
+
+def changeCoins(change):
+
+    p = 0  # 0.01
+    n = 0  # 0.05
+    d = 0  # 0.10
+    q = 0  # 0.25
+
+    changeCoins = int((change - int(change)) * 100)
+
+    if changeCoins >= 25:
+        q = int(changeCoins / 25)
+        changeCoins = changeCoins - q * 25
+
+    if changeCoins >= 10:
+        d = int(changeCoins / 10)
+        changeCoins = changeCoins - d * 10
+
+    if changeCoins >= 5:
+        n = int(changeCoins / 5)
+        changeCoins = changeCoins - n * 5
+
+    if changeCoins >= 1:
+        p = int(changeCoins / 1)
+
+    print('\nPlease give the customer the following coins\n\
+Quarters:', q, 'Dimes:', d, 'Nickels:', n, 'Pennies: ', p)
+
+
+def changeBill(change):
+
+    changeBills = int(change)
+
+    hun = 0
+    fif = 0
+    twe = 0
+    ten = 0
+    one = 0
+
+    if changeBills >= 100:
+        hun = int(changeBills / 100)
+        changeBills = changeBills - hun * 100
+
+    if changeBills >= 50:
+        fif = int(changeBills / 50)
+        changeBills = changeBills - fif * 50
+
+    if changeBills >= 20:
+        twe = int(changeBills / 20)
+        changeBills = changeBills - twe * 20
+
+    if changeBills >= 10:
+        ten = int(changeBills / 10)
+        changeBills = changeBills - ten * 10
+
+    if changeBills >= 1:
+        one = int(changeBills / 1)
+
+    print('\nPlease give the customer the following bills:\n',
+          'Hundreds: ', hun, ' Fifties: ', fif,
+          ' Twenties: ', twe, ' Tens: ', ten, ' Ones: ', one, '\n', sep='')
+
+
+def changeCalc(cost, money):
+
+    if money < cost:
+
+        change = cost - money
+
+        print('\nPlease pay %.2f$ more\n', change)
+
+    else:
+
+        change = money - cost
+
+        print('\nThe change is: %.2f$\n' % change)
+
+        changeBill(change)
+        changeCoins(change)
+
+
+def main():
+
+    cost = round(float(input('Enter the cost of the purchase: ')), 2)
+    money = round(float(input('Enter the money given: ')), 2)
+
+    changeCalc(cost, money)
+
+if __name__ == '__main__':
+    main()
